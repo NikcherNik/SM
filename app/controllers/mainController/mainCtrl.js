39d95213dc39c870c9221880e6fcc3d7dace801c
Nikcher <nikolaynikcher@gmail.com>
2017-04-17 22:14:16 +0300

    main page

--- app/controllers/mainController/mainCtrl.js
+++ app/controllers/mainController/mainCtrl.js
@@ -1,5 +1,26 @@
 module.exports = function (ngModule) {
     ngModule.controller('mainCtrl',function ($rootScope,$scope, singOutService) {
-        
+
+        console.log(this)
+        this.lists = [
+            {
+                listName: 'list1',
+            },
+            {
+                listName: 'list2',
+            },
+            {
+                listName: 'list1',
+            },
+            {
+                listName: 'list2',
+            },
+            {
+                listName: 'list1',
+            },
+            {
+                listName: 'list2',
+            }
+        ]
     });
 }
